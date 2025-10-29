63b852b4dd81d8a67f21cdb00ca8edc9fb354cee
Pedro Fuentes <pedrofuentes7799@gmail.com>
2025-10-28 21:48:10 -0300

    adds more or less how should this rclone creating go

---
+++ rclone_create.sh
@@ -0,0 +1,3 @@
+rclone config create google_drive_keepass drive client_id "CLIENT_ID" client_secret "CLIENT_SECRET" scope "drive"
+
+# Recordar que hay que entrar en las advanced options y poner el root como "Pedro/KeePass"
